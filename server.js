8ff49561b3be2718a4e2ea8edd642494958a80ad
willianlay07 <willianlay07@gmail.com>
2016-12-23 00:22:28 +0800

    Initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express 	= require('express');
+var app 		= express();
+var PORT 		= 3000;
+
+var middleware 	= {
+	requireAuthentication: function (req, res, next){
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log('Request : ' + new Date().toString() + ' ' + req.method + ' ' +req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+//app.use(middleware.requireAuthentication);		// To use this middleware in every route;
+
+// app.get('/', function(req, res){
+// 	res.send('Hello Express!');
+// });
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('About Us');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+	console.log('Express Server Started in ' + PORT + '!');
+});
